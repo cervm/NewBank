cf0a805c54a9c0e5d6e3bce32f2a465b76a98cee
qyuan7 <qyuan@ic.ac.uk>
2021-03-12 13:25:16 +0000

    Ask user to try again if login failed

--- src/newbank/server/NewBankClientHandler.java
+++ src/newbank/server/NewBankClientHandler.java
@@ -31,6 +31,7 @@ public class NewBankClientHandler extends Thread {
     public void run() {
         // keep getting requests from the client and processing them
         try {
+            CustomerID customer;
             // ask for user name
             out.println("Enter Username");
             String userName = in.readLine();
@@ -39,18 +40,25 @@ public class NewBankClientHandler extends Thread {
             String password = in.readLine();
             out.println("Checking Details...");
             // authenticate user and get customer ID token from bank for use in subsequent requests
-            CustomerID customer = bank.checkLogInDetails(userName, password);
+            customer = bank.checkLogInDetails(userName, password);
             // if the user is authenticated then get requests from the user and process them
-            if (customer != null) {
-                out.println("Log In Successful. What do you want to do?");
-                while (true) {
-                    String request = in.readLine();
-                    System.out.println("Request from " + customer.getKey());
-                    String responce = bank.processRequest(customer, request);
-                    out.println(responce);
-                }
-            } else {
-                out.println("Log In Failed");
+            while(customer == null){
+                out.println("Log In Failed. Please try again:");
+                out.println("Enter Username");
+                userName = in.readLine();
+                // ask for password
+                out.println("Enter Password");
+                password = in.readLine();
+                out.println("Checking Details...");
+                // authenticate user and get customer ID token from bank for use in subsequent requests
+                customer = bank.checkLogInDetails(userName, password);
+            }
+            out.println("Log In Successful. What do you want to do?");
+            while (true) {
+                String request = in.readLine();
+                System.out.println("Request from " + customer.getKey());
+                String responce = bank.processRequest(customer, request);
+                out.println(responce);
             }
         } catch (IOException e) {
             e.printStackTrace();
