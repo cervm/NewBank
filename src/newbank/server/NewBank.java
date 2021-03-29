a5b1b835ca8f65886538caf9f07f7fd09624b103
GeorgeSWalker <george.s.walker.1994@gmail.com>
2021-03-29 20:24:20 +0100

    Amended code to return error message and added todo

--- src/newbank/server/NewBank.java
+++ src/newbank/server/NewBank.java
@@ -163,10 +163,18 @@ public class NewBank {
 
     private String requestLoan(CustomerID customer, String loanAmount, String APR, String term)  {
         Customer cust = customers.get(customer.getKey());
+        StringBuilder output = new StringBuilder();
+        try{
+            LoanMarketplace loanMarketplace = new LoanMarketplace(cust, loanAmount, APR, term);
+            //TODO: METHOD IS NOW CALLED TWICE LOOK AT REMOVING THE OTHER ONE
+            loanMarketplace.checkLoanMeetsCriteria();
+            output.append("\nLoan Submitted to Marketplace");
+        } catch (Exception e) {
+            output.append(e.getMessage());
+            e.printStackTrace();
+        }
 
-        LoanMarketplace loanMarketplace = new LoanMarketplace(cust, loanAmount, APR, term);
-
-        return "Loan Submitted to Marketplace";
+        return output.toString();
     }
 
     //FR1.2
@@ -397,7 +405,7 @@ public class NewBank {
         return stringOut.toString();
     }
 
-
+    //TODO: Please add posible inputs accepted into here
     private String help() {
         return """
                 Type in one of the following commands
